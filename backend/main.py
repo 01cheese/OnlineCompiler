7dd8743332a30647e3989db2922de9191d96838f
Vadym Zelenko <115219323+01cheese@users.noreply.github.com>
2025-04-14 13:36:24 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 import logging
-import aioredis
+import redis.asyncio as aioredis
 from fastapi import FastAPI, WebSocket, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from celery.result import AsyncResult
